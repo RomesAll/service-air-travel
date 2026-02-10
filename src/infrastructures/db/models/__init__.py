5cff0d9b586ad088c9f5efdea67c1d230584b276
RomesAll <romesky845@gmail.com>
2026-02-10 20:56:56 +0300

    feat: обновил импорты для пакетов

---
+++ src/infrastructures/db/models/__init__.py
@@ -0,0 +1,13 @@
+from .base import Base
+from .trip import Trip
+from .pass_in_trip import PassInTrip
+from .company import Company
+from .passenger import Passenger
+
+__all__ = (
+    'Base',
+    'Trip',
+    'PassInTrip',
+    'Company',
+    'Passenger',
+)
