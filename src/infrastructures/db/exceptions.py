6564bc6ad09f4893d0e68014fda244f4243c875e
RomesAll <romesky845@gmail.com>
2026-02-12 11:50:11 +0300

    feat: добавил искл. для бд

--- src/infrastructures/db/exceptions.py
+++ src/infrastructures/db/exceptions.py
@@ -4,3 +4,17 @@ class MappersNoneException(Exception):
 
     def __str__(self):
         return f'MappersNoneException: {self.message}'
+
+class RecordsNoneException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f'RecordsNoneException: {self.message}'
+
+class RecordsNoFoundException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f'RecordsNoFoundException: {self.message}'
